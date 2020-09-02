d65079c35eff5ba8f0230aa82b51ea91b7c8da58
Connor <klaassenconnor@gmail.com>
2020-09-02 13:08:57 -0400

    checking setup

--- tester.py
+++ tester.py
@@ -1,1 +1,11 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0., 10., 0.2)
+print(len(x))
+y = np.random.random(50)
+print(x)
+print(y)
+print(x * y)
+plt.scatter(x, y*x)
+plt.show()
