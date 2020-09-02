04c9614a94a07a710115b237fe439d29748434c2
Connor <klaassenconnor@gmail.com>
2020-09-02 17:34:45 -0400

    add git lfs

--- modules/analysis.py
+++ modules/analysis.py
@@ -1,7 +1,11 @@
+from multipledispatch import dispatch
+from pandas import DataFrame
 import matplotlib.pyplot as plt
+import seaborn as sns
 import math
 
 
+@dispatch(DataFrame, str)
 def feature_relationships(df, column_target):
     fig = plt.figure(figsize=(14, (len(df.columns) / 2) * 3))
     fig.suptitle('Relationships of Data')
@@ -12,4 +16,26 @@ def feature_relationships(df, column_target):
         plt.plot(df[col], target, marker='.', linestyle='none')
         plt.title(col + " vs " + column_target)
         plt.tight_layout()
+    plt.show()
+
+
+@dispatch(DataFrame)
+def feature_relationships(df):
+    fig = plt.figure(figsize=(14, ((len(df.columns) - 1) / 2) * 3))
+    fig.suptitle('Relationships of Data')
+    column_target = df.columns[len(df.columns) - 1]
+    target = df[column_target]
+    df = df.drop(column_target, axis=1)
+    for i, col in enumerate(df.columns):
+        plt.subplot(math.ceil(len(df.columns) / 2), 2, i+1)
+        plt.plot(df[col], target, marker='.', linestyle='none')
+        plt.title(col + " vs " + column_target)
+        plt.tight_layout()
+    plt.show()
+
+
+def heatmap(df):
+    fig = plt.subplots(figsize=(10, 10))
+    sns.set(font_scale=1.5)
+    sns.heatmap(df.corr(), square=True, cbar=True, annot=True, annot_kws={'size': 10})
     plt.show()
--- tester.py
+++ tester.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from modules.analysis import feature_relationships
+from modules.analysis import feature_relationships, heatmap
 
 df = pd.read_csv("resources/housing.csv")
 
 feature_relationships(df, "median_house_value")
+feature_relationships(df)
+heatmap(df)
