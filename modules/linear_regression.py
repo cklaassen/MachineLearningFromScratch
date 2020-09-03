5ea4f924e66370c80f2507c1945310edb60331f9
Connor <klaassenconnor@gmail.com>
2020-09-03 16:25:55 -0400

    Start implementing NAN check and helping methods

--- modules/linear_regression.py
+++ modules/linear_regression.py
@@ -1,20 +1,42 @@
 from multipledispatch import dispatch
 from pandas import DataFrame
-from numpy import ndarray
+from numpy import ndarray, random, insert, sum, zeros, power, isnan, delete
 
 
 class LinearRegression:
     def __init__(self, X: ndarray, y: ndarray):
-        print(X)
-        print(y)
+        self.X = insert(X, 0, 1, axis=1)
+        missing_val_iterator = 0
+        self.y = y
+        i = 0
+        nan_list = sum(isnan(self.X), axis=1)
+        print(nan_list)
+        while i < len(self.X):
+            if nan_list[i] > 0:
+                missing_val_iterator += 1
+                self.X = delete(self.X, i)
+                self.y = delete(self.y, i)
+            else:
+                print(i)
+                i += 1
+        print(missing_val_iterator)
+        missing_val_iterator = 0
+        for nan_list in (isnan(self.X)):
+            if sum(nan_list) > 0:
+                missing_val_iterator += 1
+        print(missing_val_iterator)
+        num_params = self.X.shape[1]
+        self.theta = random.random(num_params)
+        self.values = sum(self.X * self.theta, axis=1)
+        print(self.values)
+        self.error = sum(power(self.values - self.y, 2))
+        print(self.error)
 
     @classmethod
     def from_dataframe(cls, df: DataFrame):
         y = df[df.columns[len(df.columns) - 1]].to_numpy()
         df = df.drop(df.columns[len(df.columns) - 1], axis=1)
         X = df.to_numpy()
-        print(y)
-        print(X)
         cls(X, y)
 
     @classmethod
@@ -22,6 +44,15 @@ class LinearRegression:
         y = df[df.columns[target]].to_numpy()
         df = df.drop(df.columns[target], axis=1)
         X = df.to_numpy()
-        print(y)
-        print(X)
         cls(X, y)
+
+    def _calculate_result(self):
+        self.values = sum(self.X * self.theta, axis=1)
+
+    def _calculate_error(self):
+        self.error = sum(power(self.values - self.y, 2))
+
+    def _batched_gradient_descent(self, theta, alpha, num_iterations, h, X, y, n):
+        cost = zeros(num_iterations)
+
+
