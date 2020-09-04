0ad33c8d280de3b8f6fed0ae925de826de736730
Connor <klaassenconnor@gmail.com>
2020-09-04 11:57:35 -0400

    Working 2D Linear Regression

--- tester.py
+++ tester.py
@@ -6,6 +6,15 @@ from modules.linear_regression import LinearRegression
 
 df = pd.read_csv("resources/housing.csv")
 
-LinearRegression.from_dataframe(df)
+x = np.linspace(-5,5,100)
+y = 3 * x + 1
+z = 2 * y + 3 * x + 5
 
+X = np.stack((x, z), axis=-1)
 
+x = x.reshape((len(x), 1))
+print(x)
+
+lg = LinearRegression()
+lg.fit(x, y)
+lg._gradient_descent(.02, 100)
