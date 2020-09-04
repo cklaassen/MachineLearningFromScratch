3fc85909be9675953e348f9c668b8bcedeeabf2b
Connor <klaassenconnor@gmail.com>
2020-09-04 12:35:44 -0400

    Testing data might be broken

--- tester.py
+++ tester.py
@@ -6,16 +6,23 @@ from modules.linear_regression import LinearRegression
 
 df = pd.read_csv("resources/housing.csv")
 
-x = np.linspace(-5,5,100)
-y = 3 * x + 1
-z = 2 * y + 3 * x + 5
+n = 300
+x = np.arange(-n/2, n/2, 1, dtype=np.float64)
 
-y = y * np.random.random(100)
-z = z * np.random.random(100)
+m = np.random.uniform(0.2, 0.5, (n,))
+b = np.random.uniform(0, 30, (n,))
 
+y = x*m + b
 
-X = np.stack((x, z), axis=-1)
+x = x.reshape((len(x), 1))
 
+# X = np.stack((x, z), axis=-1)
+#
 lg = LinearRegression()
-lg.fit(X, y)
+lg.fit(x, y)
 lg._gradient_descent(.001, 10)
+
+y_pred = lg.theta[0] + lg.theta[1] * x
+plt.scatter(x, y)
+plt.plot(x, y_pred)
+plt.show()
